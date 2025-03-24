ab78e04136ee84d1323c2d74ffc3650f9bd29f5f
MusicBotByRTX2024 <abdoumarzouk87@gmail.com>
2025-03-24 17:37:27 +0000

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -29,7 +29,7 @@ module.exports = {
   run: async (client, interaction) => {
     try {
 
-      const supportServerLink = "https://discord.gg/y52Tjw4aNe";
+      const supportServerLink = "https://discord.gg/XhGDjkm7Eh";
       const Instagramlink = "https://www.instagram.com/the_king_streamer/";
       const Twitchlink = "https://www.twitch.tv/matrax_demah";
       const youtubeLink = "https://www.youtube.com/@MATRAXBOY";
